5c86fd44abbfbafd004c47094fd4d37ca2025a4d
Manuel Liebchen <hallo@manuelliebchen.de>
2020-08-06 18:03:42 +0200

    adding inactive button

--- src/main/java/states/MenuState.java
+++ src/main/java/states/MenuState.java
@@ -15,6 +15,7 @@ public class MenuState extends UIState {
         super(manager, context);
         drawables.add(new TextBox( new Vec2f(-50, 100),"Capture!").setFont(ResourceManager.getInstance().loadProperties(DesignProperties.class).getTitleFont()).setVerticalAlignment(ALIGNMENT.CENTER));
         clickable.add(new Button(new Vec2f(-35, 300), Button.BUTTON_TYPE.NORMAL, "Start", () -> manager.push(new GameState(manager,context, 0))).setVerticalAlignment(ALIGNMENT.CENTER));
-        clickable.add(new Button(new Vec2f(-35, 400), Button.BUTTON_TYPE.NORMAL, "Exit", manager::popAll).setVerticalAlignment(ALIGNMENT.CENTER));
+        clickable.add(new Button(new Vec2f(-35, 400), Button.BUTTON_TYPE.NORMAL, "Inactive", () -> manager.push(new GameState(manager,context, 0))).setEnabled(false).setVerticalAlignment(ALIGNMENT.CENTER));
+        clickable.add(new Button(new Vec2f(-35, 500), Button.BUTTON_TYPE.NORMAL, "Exit", manager::popAll).setVerticalAlignment(ALIGNMENT.CENTER));
     }
 }
